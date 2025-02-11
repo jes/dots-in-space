672c976dd7258f37646e0fc00fbb185622201760
James Stanley <james@incoherency.co.uk>
2025-02-11 12:49:22 +0000

    Delete nightdrive-specific stuff

--- js/scene.js
+++ js/scene.js
@@ -55,14 +55,12 @@ Scene.prototype.render = function() {
 
 Scene.prototype.project = function(pos, z, r) {
     const dy = 0.1;
-    const dx = bend(this.viewpoint.y+dy) - bend(this.viewpoint.y);
+    const dx = 0;
     const theta = Math.atan2(dx,dy);
-    const posrel1 = pos.add(new V2d(bend(pos.y),0)).sub(this.viewpoint.add(new V2d(bend(this.viewpoint.y),0)));
+    const posrel1 = pos.sub(this.viewpoint);
     const posrel = posrel1.rotate(theta);
 
-    z = z + terrain(pos.y);
-
-    const zrel = z - (this.viewz + terrain(this.viewpoint.y));
+    const zrel = z - this.viewz;
 
     // things behind the viewer are not visible
     if (posrel.y <= 0) return null;
@@ -84,11 +82,3 @@ Scene.prototype.project = function(pos, z, r) {
         r: screenr,
     };
 };
-
-function terrain(y) {
-    return 10*Math.sin(y/1000) + 5*Math.cos(y/527) + 2*Math.sin(y/219);
-}
-
-function bend(y) {
-    return 200*Math.sin(y/909) + 51*Math.cos(y/517) + 23*Math.sin(y/201);
-}
